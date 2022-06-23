0c827860617b680b37c8bcfc959e98b20d1596b1
Leon Bernstein <unlimited.2033@gmail.com>
2022-06-23 13:36:26 +0300

    added dtos

---
+++ DotNetLessons.WebApi/Models/AddressDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetLessons.WebApi.Models;
+
+public class AddressDto
+{
+    public string GalaxyName { get; set; } = null!;
+    public string PlanetName { get; set; } = null!;
+    public bool HasConsmicRadiation { get; set; }
+}
---
+++ DotNetLessons.WebApi/Models/PersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetLessons.WebApi.Models;
+
+public class PersonDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public bool IsFromEarth { get; set; }
+
+    public List<AddressDto> Addresses { get; set; } = null!;
+}
